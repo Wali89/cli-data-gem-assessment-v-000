55c6ad887d0026a09175abd6ba4b8514b4e5d38f
Wali Rahman <wali.rahman89@yahoo.com>
2018-02-11 18:04:03 +0000

    Started on scraping

--- top_ten_crypto/lib/top_ten_crypto/crypto.rb
+++ top_ten_crypto/lib/top_ten_crypto/crypto.rb
@@ -1,8 +1,26 @@
 class TopTenCrypto::Best
-  attr_accessor :name, :price, :url, :change
+  attr_accessor :name, :price, :url, :change,
+  @@all
 
   def self.scrape_chart
-    doc = Nokogiri::
+    doc = Nokogiri::HTML(open("https://coinmarketcap.com/"))
+    chart = doc.search("tbody")
+    coin_bag = []
+    chart.search("tr").each do |coin_info|
+      coin_bag << coin_info
+    end
+
+    coin_bag.each do |info|
+      coin = TopTenCrypto::Best.new
+      #does work
+      coin.name = info.css("a.currency-name-container").text
+      #does NOT work
+      coin.url = "#{info.span('href')}"
+      coin.price = info.css("a.price").text
+      binding.pry
+      @@all << coin
+    end
+
   end
 
 end
